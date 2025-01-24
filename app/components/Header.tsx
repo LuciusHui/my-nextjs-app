8c4583a3af13d901fef896d0ae2c91247c819870
Lucius <lucius03211995@gmail.com>
2025-01-24 21:52:27 +0000

    add market button, launch app button and menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,23 @@
+//handle user interactions such as clicks without any issues
+"use client";
+
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
 
 const Header = () => {
   return (
-    <AppBar position="sticky" sx={{ backgroundColor: 'transparent', boxShadow: 'none' }}>
+    <AppBar position="absolute" sx={{ top: 0, left: 0, right: 0, backgroundColor: 'transparent', boxShadow: 'none' }}>
       <Toolbar sx={{ justifyContent: 'space-between' }}>
         <Typography variant="h6" color="primary">Akashic Domains</Typography>
-        <Button variant="contained" color="primary">Market</Button>
+        
+        <div style={{ display: 'flex', alignItems: 'center', marginLeft: 'auto' }}>
+          <Button variant="text" startIcon={<HomeIcon />} sx={{ marginRight: 2, color: '#1e00a3', borderColor: '#1e00a3', '&:hover': { backgroundColor: '#e0e0e0' } }}>Market</Button>
+          <Button variant="contained" sx={{ backgroundColor: '#1e00a3', '&:hover': { backgroundColor: '#002244' } }}>Launch App</Button>
+        </div>
+        
+        <IconButton edge="end" color="primary" sx={{ ml: 2 }} onClick={() => console.log('Menu clicked')}aria-label="Menu"> <MenuIcon /></IconButton>
       </Toolbar>
     </AppBar>
   );
